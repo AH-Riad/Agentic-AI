053aa70000950f7fddb75f29934ac437780416c8
Riad <riadk910@gmail.com>
2026-07-24 21:41:14 +0600

    Add search Engine

--- agent.py
+++ agent.py
@@ -2,10 +2,14 @@ from agno.agent import Agent
 from agno.models.groq import Groq
 from dotenv import load_dotenv
 
+from agno.tools.duckduckgo import DuckDuckGoTools
+
+
 load_dotenv()
 
 def build_agent():
     agent = Agent(
+    tools=[DuckDuckGoTools()],
     model=Groq(id="llama-3.3-70b-versatile"),
     instructions = "You are a helpful and expert travel agent.",
     markdown=True,
